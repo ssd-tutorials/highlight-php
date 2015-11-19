f9f4a221bfa2ae2820eabb928ed3463ff9683eac
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-11-19 15:16:00 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# highlight-php
-Exercise files for the course 
+# Highlight keywords and dynamically create anchor tags with PHP
+Exercise files for the course **Highlight keywords and dynamically create anchor tags with PHP**
---
+++ start/index.php
@@ -0,0 +1,23 @@
+<?php
+$paragraph = "<p>Copyright is a complicated area and riddled with misunderstandings, especially in the case of internet copyright where strangely, many people tend to believe that if it's on the web, anyone can use it. However, this could not be further from the truth, as a host of lawsuits has proved. If you have a website, then you are at risk from copyright infringement in two ways. Read on to find out how, and what this could mean for your business.<p>We all know that if someone paints a picture or writes a book, the artist or author is the legal owner of that material and therefore retains the copyright. If the book is reproduced under a different name or the painting copied and sold as someone else's work, this is of course illegal.</p><p>The same is true with content published online; whoever writes it, owns it, or in the case of having your content written by a web copy writer, once you pay for it, it´s yours. And it's the same with images. You either use your own, or you buy them from a library.</p>
+<p>And so you publish your material in good faith, but later on you find that your 'unique' content or images have been published on another website. Not only is this against the law, it can also have serious repercussions where the search engines are concerned because Google will penalise your site should it appear to have the same content as another website.</p>
+<p>So, what to do? Well the first thing is to ensure you have a copyright notice on your website; this acts as a deterrent and will also protect you should a problem arise. The notice should read: &copy; 2010 Company Name All Rights Reserved and should be updated each year with the relevant date.</p><p>Within your website terms, you should have a notice stating that all the content and images on your website belong to your company and must not be reproduced without express written permission.</p><p>If you engage in a search engine optimisation project, then your project manager will soon flag up any duplicate content and then it will be over to you to take the appropriate action and issue a warning to the offending company to take down the content with immediate effect.</p>";
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>PHP Highlight</title>
+	<meta name="description" content="PHP Highlight" />
+	<meta name="keywords" content="PHP Highlight" />
+	<link href="/css/core.css" rel="stylesheet" type="text/css" />
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+
+
+</body>
+</html>
